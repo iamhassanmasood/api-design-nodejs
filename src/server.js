81348388306f2963832034dc6d4a4111e3643b2d
Hassan Masood <iamhassanmasood@gmail.com>
2022-11-17 16:07:33 +0500

    first commit

---
+++ src/server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+// import config from './config'
+import cors from 'cors'
+// import { signup, signin, protect } from './utils/auth'
+// import { connect } from './utils/db'
+// import userRouter from './resources/user/user.router'
+// import itemRouter from './resources/item/item.router'
+// import listRouter from './resources/list/list.router'
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+// app.post('/signup', signup)
+// app.post('/signin', signin)
+
+// app.use('/api', protect)
+// app.use('/api/user', userRouter)
+// app.use('/api/item', itemRouter)
+// app.use('/api/list', listRouter)
+
+export const start = async () => {
+  try {
+    // await connect()
+    app.listen(3000, () => {
+      console.log(`REST API on http://localhost:3000`)
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
